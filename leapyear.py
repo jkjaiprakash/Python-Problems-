595974939d84cc698937d2a1f98b4da1a0ef90df
jkjaiprakash <42861486+jkjaiprakash@users.noreply.github.com>
2019-02-07 01:20:29 -0800

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,9 +1,9 @@
-year = int(input("Enter Year: "))
-if year % 4 == 0 and year % 100 != 0:
+rear= int(input("Enter Year: "))
+if year % 4 == 0 and rear % 100 != 0:
     print("Yes")
-elif year % 100 == 0:
+elif rear % 100 == 0:
     print( "not")
-elif year % 400 ==0:
+elif rear % 400 ==0:
     print("Yes")
 else:
     print("not")
